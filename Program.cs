6eef888b165ce4015941cebfb9ec50e73b1fc1d8
dotnet050219 <47572589+dotnet050219@users.noreply.github.com>
2019-02-19 19:14:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1902
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// comment one line
+            ///*
+            // 1
+            // 2
+            // 3
+            // 4
+            // */
+            //Console.WriteLine("Hello World!");
+
+            //int a = 5;
+            //int b = a * 66;
+            //float f = 5.5f;
+            //double d = a * b * f;
+            //Console.WriteLine(d);
+
+            /*
+            // in 3 lines
+            int x;
+
+            // str get get a string from the user 
+            string str = Console.ReadLine(); // readline gets a string from the user
+
+            x = Convert.ToInt32(str); // coverts the str text into a string -> put it in x 
+
+            // in 1 line
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            int y = x * 2;
+
+            Console.WriteLine(y);
+
+            */
+
+            // read number print ugi / e-zugi
+
+            Console.Write("Please enter a number (zugi check): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                // YES
+                Console.WriteLine("the number is zugi");
+            }
+            else
+            {
+                // NO
+                Console.WriteLine("the number is e zugi");
+            }
+
+
+
+
+        }
+    }
+}
